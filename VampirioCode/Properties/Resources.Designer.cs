28c23e823addcfcda13ee30a0a083aecc849c2fb
capitan <capitan@gmail.com>
2024-02-01 15:43:36 -0300

    update images

--- VampirioCode/Properties/Resources.Designer.cs
+++ VampirioCode/Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace VampirioCode.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap logo_mini {
+            get {
+                object obj = ResourceManager.GetObject("logo_mini", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap play_icon {
             get {
                 object obj = ResourceManager.GetObject("play_icon", resourceCulture);
